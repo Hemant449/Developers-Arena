5e6ebee2ab7ff0f0db239af66809c3eed7954126
Hemant449 <128729531+Hemant449@users.noreply.github.com>
2026-09-16 19:54:22 +0530

    Add files via upload

---
+++ Task1/intro.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+print()
+print(f"🎉 Welcome {name}! 🎉")
+print(f"You are {age} years old and love {hobby}.")
